38f8a84f3fb517dc834c16e7378fbbe3f27e2610
achimvp <prittwitza@gmail.com>
2026-05-07 12:10:15 +0200

    refactor(project structure): Hierarchical project structure with src, tests and examples

---
+++ src/structs/structs.hpp
@@ -0,0 +1,26 @@
+#pragma once
+#include <iostream>
+#include <vector>
+
+struct Person
+{
+    std::string name;
+    int age;
+
+    // Constructor
+    Person(const std::string& n, int a) : name(n), age(a) {}
+};
+
+std::ostream& operator<<(std::ostream& os, const Person& person);
+
+Person oldest(const std::vector<Person>& persons);
+
+struct Team
+{
+    std::string name;
+    std::vector<Person> members;
+
+    // Constructor
+    Team(const std::string& n, const std::vector<Person>& ms) : name(n), members(ms) {}
+};
+std::ostream& operator<<(std::ostream& os, const Team& team);
